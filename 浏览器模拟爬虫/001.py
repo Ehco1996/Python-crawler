64593bb1f0ffd6d79712c037cd6a0cd14cb7d3bb
ehco1996 <zh19960202@gmail.com>
2017-05-23 20:08:19 +0800

    Selenium和PhantomJS的安装与初步试用

--- 浏览器模拟爬虫/001.py
+++ 浏览器模拟爬虫/001.py
@@ -1,1 +1,19 @@
-import 
+'''
+测试selenium模拟浏览器
+和phantomjs无页面浏览器
+
+导出PhantomJs浏览器帮助文档
+'''
+
+from selenium import webdriver
+import sys
+
+browser = webdriver.PhantomJS()
+out = sys.stdout
+
+sys.stdout = open('browserHelp.txt','w')
+help(browser)
+sys.stdout.close()
+sys.stdout = out
+browser.quit()
+exit()
