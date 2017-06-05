23272554d82719233b735a09d6906669b77e809e
ehco1996 <zh19960202@gmail.com>
2017-06-05 17:57:49 +0800

    增加注释，删除个人信息

--- requestes基本使用/login.py
+++ requestes基本使用/login.py
@@ -14,12 +14,11 @@ def get_post_data(url):
 
     # 找到form的验证参数
     __VIEWSTATE = soup.find('input', attrs={'name': '__VIEWSTATE'})['value']
-    __VIEWSTATEGENERATOR = soup.find(
-        'input', attrs={'name': '__VIEWSTATEGENERATOR'})['value']
+    
 
     # 下载验证码图片
     pic = requests.get(
-        'http://jw.usts.edu.cn/(gxv2le55n4jswm45mkv14o2n)/CheckCode.aspx').content
+        'http://jw.***.edu.cn/(gxv2le55n4jswm45mkv14o2n)/CheckCode.aspx').content
     with open('ver_pic.png', 'wb') as f:
         f.write(pic)
 
@@ -27,12 +26,13 @@ def get_post_data(url):
     image = Image.open('{}/ver_pic.png'.format(os.getcwd()))
     image.show()
 
+    # 构造需要post的参数表
     data = {'txtUserName': '',
             'Textbox1': '',
             'TextBox2': '',
             'txtSecretCode': "",
             '__VIEWSTATE': '',
-            '__VIEWSTATEGENERATOR': '11',
+            # 这里我将radio栏--学生 encode成gbk编码，以符合数据的要求
             'RadioButtonList1': '\xd1\xa7\xc9\xfa',
             'Button1': '',
             'lbLanguage': '',
@@ -41,7 +41,6 @@ def get_post_data(url):
 
     # 构造登录的post参数
     data['__VIEWSTATE'] = __VIEWSTATE
-    data['__VIEWSTATEGENERATOR'] = __VIEWSTATEGENERATOR
     data['txtSecretCode'] = input('请输入图片中的验证码')
     data['txtUserName'] = input("请输入学号")
     data['TextBox2'] = input("请输入密码")
@@ -51,20 +50,21 @@ def get_post_data(url):
 
 # 登录教务系统
 def login(url,data):
+    # 通过requests库构造一个浏览器session，这能帮我们自动、持久的管理cookies，
     s = requests.session()
     s.post(url, data=data)
     return s
 
 
 
-base_url = 'http://jw.usts.edu.cn/(gxv2le55n4jswm45mkv14o2n)/default2.aspx'
+base_url = 'http://jw.****.edu.cn/(gxv2le55n4jswm45mkv14o2n)/default2.aspx'
 data = get_post_data(base_url)
 print(data)
 # 模拟登录教务系统
 brow = login(base_url,data)
 
 test = brow.get(
-    'http://jw.usts.edu.cn/(gxv2le55n4jswm45mkv14o2n)/xs_main.aspx?xh=14200406101')
+    'http://jw.****.edu.cn/(gxv2le55n4jswm45mkv14o2n)/xs_main.aspx?xh=14200406101')
 
 # 测试看看是否能找到登陆后的信息
 soup = bs4.BeautifulSoup(test.text, 'lxml')
